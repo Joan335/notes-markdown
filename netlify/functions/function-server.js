0fa734f573b6478167fc040590d1c7c9dd326ed6
joan <joan.calderon@correounivalle.edu.co>
2026-03-08 20:34:59 -0500

    hola

--- netlify/functions/function-server.js
+++ netlify/functions/function-server.js
@@ -1,6 +1,3 @@
-// netlify/functions/notasMD.js
-const fetch = require('node-fetch'); // Si usas Node < 18
-
 exports.handler = async function (event, context) {
     const TOKEN = process.env.TOKEN_API; // Netlify inyectará esto aquí de forma segura
     const urlCarpeta = "https://api.github.com/repos/Joan335/notes-markdown/contents/notes/";
